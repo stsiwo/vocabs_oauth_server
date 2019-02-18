a80d5a542a2bb8fb28c092ed31f3c1e17ca4ed5c
sts-msys <stsiwo@gmail.com>
2019-02-18 00:39:54 -0800

    added jest config and tsconfig

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/tests"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "\\.(spec|test)\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+}
+
